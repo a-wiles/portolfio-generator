6c7175898f93e5b476364ada29d458bfe910ef9f
Alex Wiles <alexandra.wiles6@gmail.com>
2022-03-28 23:04:19 -0400

    Lesson 1 code

---
+++ app.js
@@ -0,0 +1,4 @@
+const profileDataArg = process.argv.slice(2, process.argv.length);
+console.log(profileDataArg);
+
+const printProfileData = profileDataArr.forEach(profileItem => console.log(profileItem));
